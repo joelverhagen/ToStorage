1f86f147d76f743a54d36599191e3e9910727b0d
Joel Verhagen <jver@microsoft.com>
2016-01-27 15:36:49 -0800

    Initial commit

---
+++ ToStorage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Knapcode.ToStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ ToStorage/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Knapcode.ToStorage")]
+[assembly: ComVisible(false)]
+[assembly: Guid("19eef4ea-0d25-470b-a1d4-84a9cbbd8eaf")]
