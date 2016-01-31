62772d9ede3d89d0991dcd5a1c41fc6c1e04a450
Joel Verhagen <joel.verhagen@gmail.com>
2016-01-31 14:32:10 -0800

    Start working on build script

---
+++ build/build.ps1
@@ -0,0 +1,30 @@
+# constants
+$msbuildPath = Join-Path ${env:ProgramFiles(x86)} "MSBuild\14.0\Bin\msbuild.exe"
+$nugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+$ilmergePattern = "packages\**\ilmerge.exe"
+$solutionFileName = "ToStorage.sln"
+
+# build paths
+$buildPath = $PSScriptRoot
+$rootPath = Join-Path $buildPath ".."
+$nugetPath = Join-Path $buildPath "nuget.exe"
+$solutionPath = Join-Path $rootPath $solutionFileName
+
+# download nuget.exe
+If (-Not (Test-Path $nugetPath)) {
+	Invoke-WebRequest $nugetUrl -OutFile $nugetPath
+}
+
+# install packages
+& $nugetPath restore $solutionPath -SolutionDirectory $rootPath
+
+# find ilmerge
+$ilmergePath = Get-ChildItem (Join-Path $rootPath $ilmergePattern) -Recurse
+if (!$ilmergePath) {
+    throw "The build script could not find ilmerge.exe"
+}
+
+$ilmergePath = $ilmergePath.FullName
+
+# build
+& $msbuildPath $solutionPath /t:Rebuild /p:Configuration=Release
