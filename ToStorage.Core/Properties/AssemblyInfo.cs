8d111b74dccb5a60b8931a465c5fed751dba5207
Joel Verhagen <joel.verhagen@gmail.com>
2016-01-31 12:16:15 -0800

    Add Core project

---
+++ ToStorage.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Knapcode.ToStorage.Core")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6ce61b1d-4bec-4734-ae09-299c816202bb")]
