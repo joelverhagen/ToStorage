4208ca59067a57d91c78987d4453371348f76d60
Joel Verhagen <joel.verhagen@gmail.com>
2016-01-31 16:20:29 -0800

    Finish build script

--- build/build.ps1
+++ build/build.ps1
@@ -1,14 +1,14 @@
 # constants
+$version = "1.0.0"
 $msbuildPath = Join-Path ${env:ProgramFiles(x86)} "MSBuild\14.0\Bin\msbuild.exe"
 $nugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-$ilmergePattern = "packages\**\ilmerge.exe"
-$solutionFileName = "ToStorage.sln"
 
 # build paths
 $buildPath = $PSScriptRoot
 $rootPath = Join-Path $buildPath ".."
 $nugetPath = Join-Path $buildPath "nuget.exe"
-$solutionPath = Join-Path $rootPath $solutionFileName
+$solutionPath = Join-Path $rootPath "ToStorage.sln"
+$artifactsPath = Join-Path $rootPath "artifacts"
 
 # download nuget.exe
 If (-Not (Test-Path $nugetPath)) {
@@ -20,7 +20,7 @@ If (-Not (Test-Path $nugetPath)) {
 & $nugetPath restore (Join-Path $buildPath "packages.config") -SolutionDirectory $rootPath
 
 # find ilmerge
-$ilmergePath = Get-ChildItem (Join-Path $rootPath $ilmergePattern) -Recurse
+$ilmergePath = Get-ChildItem (Join-Path $rootPath "packages\**\ilmerge.exe") -Recurse
 if (!$ilmergePath) {
     throw "The build script could not find ilmerge.exe"
 }
@@ -28,6 +28,23 @@ if (!$ilmergePath) {
 $ilmergePath = $ilmergePath.FullName
 
 # build
-& $msbuildPath $solutionPath /t:Rebuild /p:Configuration=Debug
-& $msbuildPath $solutionPath /t:Rebuild /p:Configuration=Release
+& $msbuildPath $solutionPath /t:Build /p:Configuration=Release
 
+if (-Not (Test-Path $artifactsPath)) {
+    New-Item -Path $artifactsPath -ItemType directory
+}
+
+# ilmerge
+$toolOutput = "ToStorage.Tool\bin\Release"
+$unmergedExePath = (Get-ChildItem (Join-Path $rootPath (Join-Path $toolOutput "*.exe")) | Select-Object -First 1).FullName
+$dependencies = Get-ChildItem (Join-Path $rootPath (Join-Path $toolOutput "*.dll")) | Select-Object -ExpandProperty FullName
+$ilmergeArguments = "/ndebug", ("/ver:" + $version + ".0"), ("/out:" + (Join-Path $artifactsPath "ToStorage.exe")), $unmergedExePath
+$ilmergeArguments += $dependencies
+
+& $ilmergePath $ilmergeArguments
+
+# NuGet pack Core
+& $nugetPath pack (Join-Path $rootPath "ToStorage.Core\ToStorage.Core.csproj") -OutputDirectory $artifactsPath -Version $version -Prop Configuration=Release
+
+# NuGet pack Tool
+& $nugetPath pack (Join-Path $rootPath "ToStorage.Tool\Knapcode.ToStorage.Tool.nuspec") -OutputDirectory $artifactsPath -Version $version -BasePath $rootPath
