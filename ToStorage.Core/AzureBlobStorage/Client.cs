515ab1d11cdd58b50da4f6ad31c77b5f9ddf7840
Joel Verhagen <jver@microsoft.com>
2016-02-25 08:31:48 -0800

    Add GetLatestStreamAsync to Client

--- ToStorage.Core/AzureBlobStorage/Client.cs
+++ ToStorage.Core/AzureBlobStorage/Client.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
@@ -10,79 +11,133 @@ namespace Knapcode.ToStorage.Core.AzureBlobStorage
     {
         public async Task<UploadResult> UploadAsync(string account, string key, UploadRequest request)
         {
-            var storageCredentials = new StorageCredentials(account, key);
-            var cloudStorageAccount = new CloudStorageAccount(storageCredentials, true);
-            return await UploadAsync(cloudStorageAccount, request).ConfigureAwait(false);
+            var context = new CloudContext(account, key, request.Container);
+            return await UploadAsync(context, request).ConfigureAwait(false);
         }
 
         public async Task<UploadResult> UploadAsync(string connectionString, UploadRequest request)
         {
-            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
-            return await UploadAsync(cloudStorageAccount, request).ConfigureAwait(false);
+            var context = new CloudContext(connectionString, request.Container);
+            return await UploadAsync(context, request).ConfigureAwait(false);
         }
 
-        private async Task<UploadResult> UploadAsync(CloudStorageAccount account, UploadRequest options)
+        public async Task<Stream> GetLatestStreamAsync(string account, string key, GetLatestRequest request)
+        {
+            var context = new CloudContext(account, key, request.Container);
+            return await GetLatestStreamAsync(request, context);
+        }
+
+        public async Task<Stream> GetLatestStreamAsync(string connectionString, GetLatestRequest request)
+        {
+            var context = new CloudContext(connectionString, request.Container);
+            return await GetLatestStreamAsync(request, context);
+        }
+
+        private static async Task<Stream> GetLatestStreamAsync(GetLatestRequest request, CloudContext context)
+        {
+            if (!await context.BlobContainer.ExistsAsync())
+            {
+                request.Trace.WriteLine($"Blob container '{context.BlobContainer.Name}' in account '{context.StorageAccount.Credentials.AccountName}' does not exist.");
+                return null;
+            }
+
+            var latestPath = GetLatestPath(request.PathFormat);
+            var latestBlob = context.BlobContainer.GetBlockBlobReference(latestPath);
+
+            if (!await latestBlob.ExistsAsync())
+            {
+                request.Trace.WriteLine($"No blob exists at '{latestBlob.Uri}'.");
+            }
+
+            return await latestBlob.OpenWriteAsync();
+        }
+
+        private async Task<UploadResult> UploadAsync(CloudContext context, UploadRequest request)
         {
             // initialize
-            var trace = options.Trace;
-            options.Trace.Write("Initializing...");
+            request.Trace.Write("Initializing...");
 
-            var cloudBlobClient = account.CreateCloudBlobClient();
-            var cloudBlobContainer = cloudBlobClient.GetContainerReference(options.Container);
-
-            await cloudBlobContainer.CreateIfNotExistsAsync(
+            await context.BlobContainer.CreateIfNotExistsAsync(
                 BlobContainerPublicAccessType.Blob,
                 new BlobRequestOptions(),
                 null).ConfigureAwait(false);
-            trace.WriteLine(" done.");
+            request.Trace.WriteLine(" done.");
 
-            string directPath = string.Format(options.PathFormat, DateTimeOffset.UtcNow.ToString("yyyy.MM.dd.HH.mm.ss"));
-            trace.Write($"Uploading the blob to {directPath}...");
-            var directBlob = cloudBlobContainer.GetBlockBlobReference(directPath);
+            var directPath = string.Format(request.PathFormat, DateTimeOffset.UtcNow.ToString("yyyy.MM.dd.HH.mm.ss"));
+            request.Trace.Write($"Uploading the blob to '{directPath}'...");
+            var directBlob = context.BlobContainer.GetBlockBlobReference(directPath);
 
             // upload the blob
             using (var blobStream = await directBlob.OpenWriteAsync().ConfigureAwait(false))
             {
-                await options.Stream.CopyToAsync(blobStream).ConfigureAwait(false);
+                await request.Stream.CopyToAsync(blobStream).ConfigureAwait(false);
             }
-
-            trace.WriteLine(" done.");
+            request.Trace.WriteLine(" done.");
 
             // set the content type
-            if (!string.IsNullOrWhiteSpace(options.ContentType))
+            if (!string.IsNullOrWhiteSpace(request.ContentType))
             {
-                trace.Write("Setting the content type...");
-                directBlob.Properties.ContentType = options.ContentType;
+                request.Trace.Write("Setting the content type...");
+                directBlob.Properties.ContentType = request.ContentType;
                 await directBlob.SetPropertiesAsync().ConfigureAwait(false);
-                trace.WriteLine(" done.");
+                request.Trace.WriteLine(" done.");
             }
 
             // set the latest
             CloudBlockBlob latestBlob = null;
-            if (options.UpdateLatest)
+            if (request.UpdateLatest)
             {
-                var latestPath = string.Format(options.PathFormat, "latest");
-                trace.Write($"Updating {latestPath} to the latest blob...");
-                latestBlob = cloudBlobContainer.GetBlockBlobReference(latestPath);
+                var latestPath = GetLatestPath(request.PathFormat);
+                request.Trace.Write($"Updating '{latestPath}' to the latest blob...");
+                latestBlob = context.BlobContainer.GetBlockBlobReference(latestPath);
                 await latestBlob.StartCopyAsync(directBlob).ConfigureAwait(false);
                 while (latestBlob.CopyState.Status == CopyStatus.Pending)
                 {
                     await Task.Delay(100).ConfigureAwait(false);
                 }
-                trace.WriteLine(" done.");
+                request.Trace.WriteLine(" done.");
             }
 
             var result = new UploadResult {DirectUrl = directBlob.Uri};
-
-            trace.WriteLine();
-            trace.WriteLine($"Direct: {directBlob.Uri}");
+            request.Trace.WriteLine();
+            request.Trace.WriteLine($"Direct: {directBlob.Uri}");
             if (latestBlob != null)
             {
                 result.LatestUrl = latestBlob.Uri;
-                trace.WriteLine($"Latest: {latestBlob.Uri}");
+                request.Trace.WriteLine($"Latest: {latestBlob.Uri}");
             }
 
             return result;
+        }
+
+        private static string GetLatestPath(string pathFormat)
+        {
+            var latestPath = string.Format(pathFormat, "latest");
+            return latestPath;
+        }
+
+        private class CloudContext
+        {
+            private CloudContext(string container)
+            {
+                BlobClient = StorageAccount.CreateCloudBlobClient();
+                BlobContainer = BlobClient.GetContainerReference(container);
+            }
+
+            public CloudContext(string connectionString, string container) : this(container)
+            {
+                StorageAccount = CloudStorageAccount.Parse(connectionString);
+            }
+
+            public CloudContext(string account, string key, string container) : this(container)
+            {
+                var storageCredentials = new StorageCredentials(account, key);
+                StorageAccount = new CloudStorageAccount(storageCredentials, true);
+            }
+
+            public CloudStorageAccount StorageAccount { get; }
+            public CloudBlobClient BlobClient { get; }
+            public CloudBlobContainer BlobContainer { get; }
         }
     }
 }
---
+++ ToStorage.Core/AzureBlobStorage/GetLatestRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace Knapcode.ToStorage.Core.AzureBlobStorage
+{
+    public class GetLatestRequest
+    {
+        public string Container { get; set; }
+        public string PathFormat { get; set; }
+        public TextWriter Trace { get; set; }
+    }
+}
