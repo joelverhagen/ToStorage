7eee11f4c8a0f30b5f8f3415693e254e06c749e0
Joel Verhagen <jver@microsoft.com>
2016-02-25 08:34:28 -0800

    Add skeleton test project

---
+++ ToStorage.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ToStorage.Core.Tests")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9d17385a-3b86-4923-a11e-c26a39238002")]
